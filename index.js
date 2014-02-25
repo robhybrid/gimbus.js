7329c04a1b075ddd16404cb03207545bfda0c9ff
Ashley the Catgirl <me@ashley.re>
2014-02-25 17:19:51 +0100

    Added code.

---
+++ index.js
@@ -0,0 +1,35 @@
+var request = require('request');
+function shorten(url,callback) {
+	var callback = typeof callback !== 'undefined' ? callback : console.log;
+	request(
+		{
+			uri: "http://gimb.us/",
+			method: "GET",
+			qs: {
+				url: url
+			}
+		},
+		
+		function(error, response, body) {
+			callback("http://gimb.us/"+body)
+		}
+	);
+}
+
+function unshorten(url,callback) {
+	var callback = typeof callback !== 'undefined' ? callback : console.log
+	request(
+		{
+			uri: url,
+			method: "HEAD",
+			followRedirect: false
+		},
+		
+		function(error, response, body) {
+			callback(response.headers['location'])
+		}
+	);
+}
+
+module.exports.shorten=shorten;
+module.exports.unshorten=unshorten;
